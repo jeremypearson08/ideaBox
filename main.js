81fd69d222ae724fb4f0314e6bc3a9e15d1fde83
Jeremy Pearson <jeremy.pearson08@gmail.com>
2023-09-12 14:50:13 -0600

    Added querySelectors

---
+++ main.js
@@ -0,0 +1,17 @@
+//DOM Elements
+var saveButton = document.querySelector(".save-button");
+var titleInput = document.querySelector("#title");
+var bodyInput = docutment.querySelector("#body");
+
+//Event Listeners
+saveButton.addEventListener("click", )
+
+ideas = []
+title = [];
+body = [];
+
+//Functions
+function () {
+    if 
+}
+
