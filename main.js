dc3023b3ef41efe0c04549e5c63da71e28217d5b
Jeremy Pearson <jeremy.pearson08@gmail.com>
2023-09-12 17:23:53 -0600

    Added render function

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 var saveButton = document.querySelector(".save-button");
 var titleInput = document.querySelector("#title");
 var bodyInput = document.querySelector("#body");
+var cardTitle = document.querySelector(".card-title");
+var cardBody = document.querySelector(".card-body");
 
 //Event Listeners
 saveButton.addEventListener("click", saveIdea);
@@ -18,18 +20,19 @@ function createNewIdea (title, body) {
     return newIdea;
 } 
 
+function renderIdea(idea) {
+    cardTitle.innerHTML = idea.title;
+    cardBody.innerHTML = idea.body;
+}
+
 function saveIdea (e) {
     e.preventDefault()
     var title = titleInput.value;
     var body = bodyInput.value;
     var newObject = createNewIdea(title, body);
     ideas.push(newObject);
-
+    renderIdea(newObject)
     titleInput.value = "";
     bodyInput.value = "";
 }
-
-//function displayIdeas () {
-
-//}
 
