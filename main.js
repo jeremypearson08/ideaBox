d8725afddd84e66b5c0a758ee4002d1d871a0d10
jayceeannne <jayceer84@gmail.com>
2023-09-12 18:50:25 -0700

    feature/add ideas

--- main.js
+++ main.js
@@ -4,26 +4,29 @@ var titleInput = document.querySelector("#title");
 var bodyInput = document.querySelector("#body");
 var cardTitle = document.querySelector(".card-title");
 var cardBody = document.querySelector(".card-body");
+var addNewIdea = document.querySelector('.add-new');
+var ideaContainer = document.querySelector('.ideaContainer');
+var allIdeaCards = document.querySelectorAll('.ideaContainer .idea-card');
 
 //Event Listeners
-saveButton.addEventListener("click", saveIdea);
+addNewIdea.addEventListener('click', saveIdea); 
 
+//Global Variables
 var ideas = [];
 
 //Functions
 function createNewIdea (title, body) {
     var newIdea = {
-        title: title, 
+        title: title,
         body: body,
         id: Date.now()
     };
     return newIdea;
-} 
-
-function renderIdea(idea) {
-    cardTitle.innerHTML = idea.title;
-    cardBody.innerHTML = idea.body;
 }
+//function renderIdea(idea) {
+//    cardTitle.innerHTML = idea.title;
+//   cardBody.innerHTML = idea.body;
+//}
 
 function saveIdea (e) {
     e.preventDefault()
@@ -31,8 +34,17 @@ function saveIdea (e) {
     var body = bodyInput.value;
     var newObject = createNewIdea(title, body);
     ideas.push(newObject);
-    renderIdea(newObject)
+    createIdeaCard(newObject)
     titleInput.value = "";
     bodyInput.value = "";
 }
-
+function createIdeaCard(idea) {
+    var ideaDiv = document.createElement('div');
+    ideaDiv.className = 'idea-card';
+      ideaDiv.innerHTML = `
+      <div class="idea-box">
+      <p><strong>${idea.title}</strong></p>
+      <p>${idea.body}</p>
+    `;
+    ideaContainer.appendChild(ideaDiv);
+  }
