e139bfc08118a42134593e36071e923cda5a313d
Eric Kendrick <eric.c.kendrick@gmail.com>
2023-09-17 15:33:41 -0500

    Rename close-btn to close-button, format to soft tabs, add new function to toggle star icon

--- main.js
+++ main.js
@@ -18,101 +18,101 @@
     favoritesButton.addEventListener('click', function() {
       toggleFavButtonView();
       toggleIdeasContainer();
-    })
+    });
 
 //Functions
     function createIdea(title, body) {
-        return {
-            title: title,
-            body: body,
-            id: Date.now(),
-            isStarred: false
-        };
-    }
+      return {
+        title: title,
+        body: body,
+        id: Date.now(),
+        isStarred: false
+      };
+    };
 
     function createIdeaCard(e) {
-        e.preventDefault();
-        currentCard = createIdea(titleInput.value, bodyInput.value);
-        ideas.push(currentCard);
-        displayCards(ideas);
-        formValueReset();
-        buttonDisabledState();  
-    }
+      e.preventDefault();
+      currentCard = createIdea(titleInput.value, bodyInput.value);
+      ideas.push(currentCard);
+      displayCards(ideas);
+      formValueReset();
+      buttonDisabledState();  
+    };
 
     function displayCards(ideas) {
-        ideaContainer.innerHTML = '';
-        ideas.forEach(function(idea) {
-            ideaContainer.innerHTML += 
-            `<article class='idea-card'>
-                <div class='idea-header'>
-                    <img class='star-icon' src='${starIconView(idea)}'</img>
-                    <button class='close-button'>&#9587;</button>
-                </div>
-                <p class='idea-title'>${idea.title}</p>
-                <p>${idea.body}</p>
-            </article>`;
-        });
-    }
+      ideaContainer.innerHTML = '';
+      ideas.forEach(function(idea) {
+      ideaContainer.innerHTML += 
+        `<article class="idea-card">
+           <div class="idea-header">
+             <img class="star-icon" src="${starIconView(idea)}"</img>
+             <button class="close-button">&#9587;</button>
+           </div>
+           <p class="idea-title">${idea.title}</p>
+           <p>${idea.body}</p>
+        </article>`;
+      });
+    };
 
     function starIconView(idea) {
-        if (idea.isStarred) {
-            return 'assets/star-active.svg';
-        }
-        return 'assets/star.svg';
-    }
+      if (idea.isStarred) {
+        return 'assets/star-active.svg';
+      };
+      return 'assets/star.svg';
+    };
 
     function cardInteractions(e) {
       var targetElement = e.target;
       if (targetElement.tagName.toLowerCase() === 'path') {
-          targetElement = targetElement.parentElement;
+        targetElement = targetElement.parentElement;
       }
       if (targetElement.classList.contains('close-button')) {
-          deleteCard(e);
+        deleteCard(e);
       } else if (targetElement.classList.contains('star-icon')) {
-          toggleStar(e);
-      }
-    }
+        toggleStar(e);
+      };
+    };
 
     function deleteCard(e) {
-        var clickedBtn = e.target;
-        var ideaCard = clickedBtn.closest('.idea-card');
-        if (ideaCard) {
-            var cardIndex = Array.from(ideaContainer.children).indexOf(ideaCard);
-            if (cardIndex !== -1) {
-                ideas.splice(cardIndex, 1);
-                displayCards(ideas);
-            }
-        }
-    }
+      var clickedBtn = e.target;
+      var ideaCard = clickedBtn.closest('.idea-card');
+      if (ideaCard) {
+        var cardIndex = Array.from(ideaContainer.children).indexOf(ideaCard);
+        if (cardIndex !== -1) {
+          ideas.splice(cardIndex, 1);
+          displayCards(ideas);
+        };
+      };
+    };
 
     function toggleStar(e) {
       var clickedStar = e.target;
       var ideaCard = clickedStar.closest('.idea-card');
       if (ideaCard) {
-          var cardIndex = Array.from(ideaContainer.children).indexOf(ideaCard);
-          if (ideas[cardIndex].isStarred) {
-              ideas[cardIndex].isStarred = false;
-              clickedStar.classList.remove('favorited');
-          } else {
-              ideas[cardIndex].isStarred = true;
-              clickedStar.classList.add('favorited');
-          }
-          displayCards(ideas);
-      }
-    }
+        var cardIndex = Array.from(ideaContainer.children).indexOf(ideaCard);
+        if (ideas[cardIndex].isStarred) {
+          ideas[cardIndex].isStarred = false;
+          clickedStar.classList.remove('favorited');
+        } else {
+          ideas[cardIndex].isStarred = true;
+          clickedStar.classList.add('favorited');
+          };
+        displayCards(ideas);
+      };
+    };
 
     function buttonEnabledState() {
-        addNewIdea.removeAttribute('disabled', false);
-    }
+      addNewIdea.removeAttribute('disabled', false);
+    };
 
     function buttonDisabledState() {
-        addNewIdea.setAttribute('disabled', true);
-    }
+      addNewIdea.setAttribute('disabled', true);
+    };
 
     function formValueReset() {
-        titleInput.value = '';
-        bodyInput.value = '';
-    }
+      titleInput.value = '';
+      bodyInput.value = '';
+    };
 
     function toggleFavButtonView() {
       if (favView) {
@@ -121,8 +121,8 @@
       } else {
         favView = true;
         favoritesButton.innerText = 'Show All Ideas';
-      }
-    }
+      };
+    };
 
     function toggleIdeasContainer() {
       ideaContainer.innerHTML = '';
@@ -131,10 +131,10 @@
         for (let i = 0; i < ideas.length; i++) {
           if (ideas[i].isStarred) {
             favIdeas.push(ideas[i]);
-          }
-        }
+          };
+        };
         displayCards(favIdeas);
-      } else {
+        } else {
         displayCards(ideas);
-    }
-    }
+      };
+    };
